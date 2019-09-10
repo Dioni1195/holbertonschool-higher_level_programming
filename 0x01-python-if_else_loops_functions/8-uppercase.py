95d01c20427bb536f3cf7aa15f254e7f2bb140b2
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-10 23:52:18 +0000

    Fix Feature : Fix new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         check_lower = ord(str[i])
-        if check_lower >= 97 and check_lower < 123:
+        if check_lower >= 97 and check_lower <= 122:
             check_lower -= 32
-        if i != len(str) - 1:
-            print("{}".format(chr(check_lower)), end="")
-        else:
-            print("{}".format(chr(check_lower)))
+        print("{}".format(chr(check_lower)), end="")
+    print()
