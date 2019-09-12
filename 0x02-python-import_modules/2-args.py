0b0f58076f870105a11940f7af5e4a0f11321255
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-12 18:07:50 +0000

    fix Feature : Fix one-arguments case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,11 @@ import sys
 if __name__ == "__main__":
     leng = len(sys.argv)
     if leng >= 2:
-        print("{} arguments:".format(leng - 1))
+        if leng == 2:
+            arg = "argument"
+        else:
+            arg = "arguments"
+        print("{} {}:".format(leng - 1, arg))
         for i in range(1, leng):
                 cont = i
                 print("{}: {}".format(cont, sys.argv[i]))
