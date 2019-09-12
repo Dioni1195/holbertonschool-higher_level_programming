6ae8c92edbcf4fbe9785f6436523b4e8e0ba623a
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-12 01:58:13 +0000

    Fix Feature : Fixing Format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 65):
-    print("{}".format(chr(i)), end="")
-    i -= 32
+for i in range(122, 96,-1):
+    if i % 2 != 0:
+        check_value = 32
+    else:
+        check_value = 0
+    print("{:c}".format(i - check_value), end="")
