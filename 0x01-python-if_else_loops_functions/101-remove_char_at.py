c56c83b4ee0b242b87e939c0fc9f8f9b2c466952
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-12 02:21:17 +0000

    Fix Feature : Fix assing value of a string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def remove_char_at(str2, n):
-    str1 = str2
-    for i in range(0, len(str1)):
-        if i == n:
-            str1[i] = ""
+def remove_char_at(str, n):
+    if n >= 0:
+        str1 = str[0:n] + str[n + 1:]
+    else:
+        str1 = str
     return str1
