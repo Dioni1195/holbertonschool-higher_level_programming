c4aea8a161dcf53459526d205c5eabeabd4b6b12
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-17 21:05:49 +0000

    Fix None case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if len(my_list) is 0:
         return None
     max = my_list[0]
-    for i in range(len(my_list)):
-            if i is not len(my_list) - 1:
-                if my_list[i] > my_list[i + 1] and my_list[i] > max:
-                    max = my_list[i]
-            elif i is len(my_list) - 1 and my_list[i] > max:
-                max = my_list[i]
+    for i in my_list:
+            if i > max:
+                max = i
     return max
