db5b494d69b99cc52c5fc9d7d3591293f8179347
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-17 20:54:52 +0000

    Fix max initial

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    max = 0
+    max = my_list[0]
     for i in range(len(my_list)):
             if i is not len(my_list) - 1:
                 if my_list[i] > my_list[i + 1] and my_list[i] > max:
