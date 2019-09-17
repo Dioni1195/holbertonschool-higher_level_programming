ec3ae9da426ee3d6afecc633b94e54c742033aef
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-17 20:50:56 +0000

    Add Feature : Add feature to determine the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    max = 0
+    for i in range(len(my_list)):
+            if i is not len(my_list) - 1:
+                if my_list[i] > my_list[i + 1] and my_list[i] > max:
+                    max = my_list[i]
+            elif i is len(my_list) - 1 and my_list[i] > max:
+                max = my_list[i]
+    return max
