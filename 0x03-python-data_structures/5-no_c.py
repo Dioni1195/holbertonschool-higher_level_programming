bffe2b9f8c17d96a7b30a131f7bdbeecb4989eee
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-17 18:35:58 +0000

    Add Feature : Remoca a char from a list

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+        str1 = ""
+        for i in my_string:
+            if i is not 'c' and i is not 'C':
+                str1 += i
+        return str1
