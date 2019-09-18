a7ecf02a3b96f15390a30b10c6526e7497958d8e
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-18 03:32:35 +0000

    Fixing efficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,25 +6,23 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int len, i = 0, j;
-	char buff[1024];
+	int len, i = 0, j, buff[1024];
 	listint_t *aux = *head;
 
 	if (!head || !*head || !aux->next)
 		return (1);
 	while (aux)
 	{
-		buff[i] = (char)aux->n;
+		buff[i] = aux->n;
 		i++;
 		aux = aux->next;
 	}
 	len = i - 1;
-	j = len;
 	for (i = 0; i <= len; i++)
 	{
-		if (buff[i] != buff[j])
+		if (buff[i] != buff[len])
 			return (0);
-		j--;
+		len--;
 	}
 	return (1);
 }
