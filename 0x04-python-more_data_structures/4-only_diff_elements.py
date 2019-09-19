3c4aa2f45751687daa7320b30ec0cf110d8eeaaa
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-19 17:01:09 +0000

    Fixing condition

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if (len(set_1) != 0 and len(set_2) != 0) and (set_1 is not None and set_2 is not None):
-        return set_1 ^ set_2
-    else:
-        return {}
+    return set_1 ^ set_2
