92c77b9179cfa2f873581deb50ac52dc7bcf0631
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-20 00:51:58 +0000

    Fix logic nested map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x * x, matrix[0])), list(map(lambda y: y * y, matrix[1])), list(map(lambda z: z * z, matrix[2]))]
+    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
