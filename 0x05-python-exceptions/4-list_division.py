4ae898f5102e9823291d749e06533983c042c4b6
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-30 19:00:05 +0000

    Fix Feature : Fix finally case

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,5 +11,6 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("out of range")
             except ZeroDivisionError:
                 print("division by 0")
-            i += 1
+            finally:
+                i += 1
         return new_list
