bb113dc6e2c8169315e1d9a5d02c0aee8b161b81
Dioni1195 <dionisio.arango95@gmail.com>
2019-10-02 02:58:23 +0000

    Adding files

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module contains the class Square
+"""
+
+
+class Square:
+    """Class square to make a matrix
+       Args:
+        __size (int): The size of the square
+       Attributes:
+        __size (int): The size of the square
+
+    """
+    def __init__(self, __size=0):
+            self.size = __size
+
+    @property
+    def size(self):
+        """Property Size, the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """It calcule the area of a square
+
+            Returns:
+                The area of square, an int.
+
+        """
+        return self.__size ** 2
