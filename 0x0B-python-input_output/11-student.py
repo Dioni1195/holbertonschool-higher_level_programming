bb3d872b90f5e5e5385a2dfa4bfee5a917c7af06
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-16 16:50:07 -0500

    Add class student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module define a class Student """
+
+
+class Student:
+    """ This is a class for a student
+    args:
+    first_name(str): The first name of the student
+    last_name(str): The last name of th student
+    age(int): The age of the student
+    attrs:
+    first_name(str): The first name of the student
+    last_name(str): The last name of th student
+    age(int): The age of the student
+    """
+    
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Retrieves a dictionary 
+        representation of the class
+        """
+        return self.__dict__
