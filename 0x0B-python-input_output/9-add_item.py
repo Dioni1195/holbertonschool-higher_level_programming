894b80a5221012b5af12f54aa8aa8b3dd4f08dcc
Dioni1195 <dionisio.arango95@gmail.com>
2019-10-16 21:00:42 +0000

    Add script to add  a Json

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,6 +5,13 @@ them to a file
 import sys
 
 
-save_to_json_file = __import__('7-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file.py').load_from_json_file
-print(sys.argv)
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+args = []
+try:
+    args = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    save_to_json_file([], 'add_item.json')
+for i in sys.argv[1:]:
+    args.append(i)
+save_to_json_file(args, 'add_item.json')
