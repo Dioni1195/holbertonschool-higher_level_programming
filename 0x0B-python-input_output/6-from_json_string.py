daa70f7d4a8bbd85fe491e998d668e59ef2dd056
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-16 14:19:33 -0500

    Adding functo to decode a object

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module convert json to string """
+import json
+
+
+def from_json_string(my_str):
+    """ This function converts json to string """
+    return json.loads(my_str)
