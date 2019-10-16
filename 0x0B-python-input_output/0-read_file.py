bdace0a78dc1d91bdef01f7d470f6d113ea3b115
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-16 18:12:19 -0500

    fixing end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ This function opens and read a file """
     with open(filename, 'r', encoding='utf-8') as r_file:
-        print(r_file.read())
+        print(r_file.read(),end="")
