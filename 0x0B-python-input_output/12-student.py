95b65fa99e3de7e7a8288a817ff47b7082dd9003
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-16 17:18:08 -0500

    Add modified class of Student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" This module define a class Student """
+
+
+class Student:
+    """ This is a class for a student
+    args:
+    first_name(str): The first name of the student
+    last_name(str): The last name of th student
+    age(int): The age of the student
+    attrs:
+    first_name(str): The first name of the student
+    last_name(str): The last name of th student
+    age(int): The age of the student
+    """
+    
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary 
+        representation of the class
+        """
+        if attrs is None:
+            return self.__dict__
+        new_dict = dict()
+        for verify in self.__dict__.keys():
+            if verify in attrs:
+                new_dict[verify] = verify
+        return new_dict
