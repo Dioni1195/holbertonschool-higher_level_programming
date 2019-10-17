483d87f95ac0c11fb36a9efc21bed72a6215c19a
Dioni1195 <dionisio.arango95@gmail.com>
2019-10-17 04:29:30 +0000

    Add append function

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contain a function that append a line after other """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ This function append a line after other """
+    with open(filename, 'r+', encoding='utf-8') as r_file:
+        r_str = r_file.readlines()
+        appended_str = []
+        for search in r_str:
+            appended_str.append(search)
+            if search.find(search_string) != -1:
+                appended_str.append(new_string)
+        with open(filename, 'w', encoding='utf-8') as w_file:
+            r_file.writelines(appended_str)
