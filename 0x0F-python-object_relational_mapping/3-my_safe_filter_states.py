be956ef97d93a174c5a346e26624d2f1aa2e0228
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-13 19:29:50 +0000

    Safaing SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host=ht, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states\
-                WHERE name = '{}' ORDER BY id".format(argv[4]))
+                WHERE name = %(name)s ORDER BY id", {'name': argv[4]})
     states = cur.fetchall()
     for state in states:
         print(state)
