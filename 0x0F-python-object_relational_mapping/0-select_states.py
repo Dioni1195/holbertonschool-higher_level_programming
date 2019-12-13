d0e06233c765ce4f87f8e5a9be1ac0a877505d93
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-13 18:42:49 +0000

    Fix code to dont be excecuted when it is imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,11 @@
 import MySQLdb
 from sys import argv
 
-host = "127.0.0.1"
-my_db = MySQLdb.connect(host=host, user=argv[1], passwd=argv[2], db=argv[3])
-cur = my_db.cursor()
-cur.execute("SELECT id, name FROM states")
-states = cur.fetchall()
-for state in states:
-    print (state)
+if __name__ == "__main__":
+    ht = "127.0.0.1"
+    db = MySQLdb.connect(host=ht, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id")
+    states = cur.fetchall()
+    for state in states:
+        print (state)
