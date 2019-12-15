56358208deb861d5e5bb7b596da759419e551fd6
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-15 00:12:57 +0000

    Fixing host

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,8 +4,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    ht = "127.0.0.1"
-    db = MySQLdb.connect(host=ht, user=argv[1], passwd=argv[2],
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities\
