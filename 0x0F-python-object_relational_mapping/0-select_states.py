432c597979b5beff93297989c92c13afce945a78
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-15 00:10:19 +0000

    Fixing localhost

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,11 +4,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    ht = "127.0.0.1"
-    db = MySQLdb.connect(host=ht, user=argv[1],
+    db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id")
+    cur.execute("SELECT id, name FROM states ORDER BY id;")
     states = cur.fetchall()
     for state in states:
         print(state)
