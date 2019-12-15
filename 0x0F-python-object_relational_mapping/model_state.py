8cc0c97495691065d9e8604f773b0170c053e7b5
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-15 00:03:22 +0000

    Fixing shebang

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ This module contains the class state"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
