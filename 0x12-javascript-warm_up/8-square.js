3fc145813483fa995f749705f23366301b29096b
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-04 18:09:02 +0000

    Fix verification to NaN

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myArgv = process.argv;
-let num = myArgv[2];
-if (myArgv.length === 2) {
+let num = parseInt(myArgv[2]);
+if (isNaN(num)) {
   console.log('Missing size');
 }
 while (num > 0) {
