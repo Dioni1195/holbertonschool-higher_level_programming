f7ba8a9e8130cf23d423fd21c93fb259b80ae7d8
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-11 19:57:04 +0000

    Fix style

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,7 +7,7 @@ request(myArgv[2], function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
-    let bodyResponse = body;
+    const bodyResponse = body;
     fs.writeFile(myArgv[3], bodyResponse, 'utf8', (err) => {
       if (err) {
         console.log(err);
