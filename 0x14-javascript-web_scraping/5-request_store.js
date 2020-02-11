5012b7ac4de7cde7596e229fa4728d57cc35cfec
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-11 19:55:44 +0000

    Lore ipsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const myArgv = process.argv;
+const fs = require('fs');
+
+request(myArgv[2], function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    let bodyResponse = body;
+    fs.writeFile(myArgv[3], bodyResponse, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
