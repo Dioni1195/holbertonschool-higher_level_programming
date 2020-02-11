7f909f73ab2947f73f290709adf91a633a3f9e5c
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-11 22:14:30 +0000

    Unfinished task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const myArgv = process.argv;
-const fs = require('fs');
-const userIds = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10]
-let ret_obj = {};
-for (let user of userIds) {
-  request(myArgv[2],{qs: {userId: user, completed: true}},function (error, response, body) {
-    if (error) {
-      console.error('error:', error);
-    } else {
-      ret_obj[user] = JSON.parse(body).length;
-    }
-  });
-}
 
-setTimeout(function(){
-  console.log(ret_obj);
-},2000)
+request(myArgv[2],{qs: {completed: true}},function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    for (let item of JSON.parse(body)) {
+      console.log(item.)
+    }
+  }
+});
