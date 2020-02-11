581840d7e7201b87677d91c0c9bbbb59b3dcedce
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-11 18:41:50 +0000

    Start wars

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const myArgv = process.argv;
+if (myArgv[2] !== undefined) {
+  let movies;
+  let item;
+  let count = 0;
+  request(myArgv[2], function (error, response, body) {
+    if (error) {
+      console.error('error:', error);
+    } else {
+      movies = JSON.parse(body).results;
+      for (item of movies) {
+        if (item.characters.includes('https://swapi.co/api/people/18/')) {
+          ++count;
+        }
+      }
+      console.log(count);
+    }
+  });
+}
