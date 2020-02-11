e2bf8d69280c13bcdea25a9c96f261ed7635d38d
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-11 18:04:51 +0000

    Re fix format

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@ const request = require('request');
 const myArgv = process.argv;
 if (myArgv[2] !== undefined) {
   request(myArgv[2], function (error, response, body) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   });
 }
