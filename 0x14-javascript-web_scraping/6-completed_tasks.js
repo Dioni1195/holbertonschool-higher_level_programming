315a37bb7cc038000ee5db3a9a10092acf24bce7
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-12 04:00:08 +0000

    Fixing creation of object

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const myArgv = process.argv;
-const myObj = {
-  1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
-};
+const myObj = {};
 
 request(myArgv[2], { qs: { completed: true } }, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
+    for (const item of JSON.parse(body)) {
+      myObj[item.userId] = 0;
+    }
     for (const item of JSON.parse(body)) {
       myObj[item.userId] += 1;
     }
