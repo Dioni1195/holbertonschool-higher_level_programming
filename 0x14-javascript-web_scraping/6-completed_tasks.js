7ea2bc65c74658a4c94e84e9fc61cdb489fd7043
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-12 03:56:27 +0000

    Finish logic

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const myArgv = process.argv;
+const myObj = {
+  1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
+};
 
-request(myArgv[2],{qs: {completed: true}},function (error, response, body) {
+request(myArgv[2], { qs: { completed: true } }, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
-    for (let item of JSON.parse(body)) {
-      console.log(item.)
+    for (const item of JSON.parse(body)) {
+      myObj[item.userId] += 1;
     }
+    console.log(myObj);
   }
 });
