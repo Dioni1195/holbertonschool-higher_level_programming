0c892c25cd0e3fe4087f16a4e32f0305d323121b
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-13 15:44:44 -0500

    Api and listing title movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+$(function (){
+  let $movies = $('#list_movies');
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi.co/api/films/?format=json',
+    success: function (movies) {
+      $.each(movies.results, function(i, movie) {
+        $movies.append('<li>' + movie.title + '</li>');
+      });
+    }
+  });
+});
